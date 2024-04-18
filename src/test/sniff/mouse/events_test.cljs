45030061d21c9d8ebcce3827e97ad1d50f0bf2ef
jaketothepast <windlejacob12@gmail.com>
2024-04-18 14:53:21 -0400

    Click and drag and click tests

--- src/test/sniff/mouse/events_test.cljs
+++ src/test/sniff/mouse/events_test.cljs
@@ -5,5 +5,18 @@
 (def test-click
   (js-obj "clientX" 1 "clientY" 1))
 
+(def test-click-not-same
+  (js-obj "clientX" 1 "clientY" 2))
+
 (deftest extracts-point
   (is (= {:x 1 :y 1} (events/mouse-position test-click))))
+
+(deftest click-event
+  (let [event (do (events/handle-mouse test-click)
+                  (events/handle-mouse test-click))]
+    (is (= :click (:event event)))))
+
+(deftest click-and-drag-event
+  (let [event (do (events/handle-mouse test-click)
+                  (events/handle-mouse test-click-not-same))]
+    (is (= :click-and-drag (:event event)))))
