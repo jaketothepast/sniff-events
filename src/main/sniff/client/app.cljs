aab93fdd20aaa857357043e3355e15048de6dbad
jaketothepast <windlejacob12@gmail.com>
2024-04-18 14:36:24 -0400

    Handles mouseup and mousedown on document

--- src/main/sniff/client/app.cljs
+++ src/main/sniff/client/app.cljs
@@ -1,29 +1,20 @@
 (ns sniff.client.app
-  (:require [goog.dom :as gdom]
-            [goog.events :as gevents]
+  (:require [goog.events :as gevents]
             [clojure.math]
-            [lambdaisland.fetch :as fetch]
             [sniff.mouse.events :as mouse]))
 
 (def event-stream
   "Stream of suspicious events on page"
   (atom []))
 
-(def student-id 1)
-(def assignment-id 1)
-(def event-url "http://localhost:3000/events.json")
+(def document (.-document js/window))
 
-(defn log-event
-  "Send the event to our running backend"
-  [{:keys [event]}]
-  (prn (:body (fetch/post event-url {:query-params {:event (str event) :student_id student-id :assignment_id assignment-id}}))))
+(defn log-result [wrapped]
+  (fn [e] (js/console.log (clj->js (wrapped e)))))
 
 (defn page-setup
   "Register listeners, peform authentication, and setup the stream of events to the backend server."
   []
-  (let [html (first (gdom/getElementsByTagName "html"))]
-    (gevents/removeAll html)
-    (gevents/listen html "mousedown" mouse/handle-mouse)
-    (gevents/listen html "mouseup" (fn [e]
-                                     (-> (mouse/handle-mouse e)
-                                         log-event)))))
+  (gevents/removeAll document)
+  (gevents/listen document "mousedown" (log-result mouse/handle-mouse))
+  (gevents/listen document "mouseup" (log-result mouse/handle-mouse)))
